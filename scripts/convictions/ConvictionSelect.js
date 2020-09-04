8648a83cf59eeec059a95822e87479bb3f69e2e6
Abdurehman05 <abdukr5@gmail.com>
2020-09-04 11:33:50 -0500

    EventHub,api and notes modules added

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -8,34 +8,82 @@ import {
 } from "./ConvictionProvider.js"
 
 // Get a reference to the DOM element where the <select> will be rendered
+const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".filters__crime")
 
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", event => {
+
+    // Only do this if the `crimeSelect` element was changed
+    if (event.target.id === "crimeSelect") {
+        // Create custom event. Provide an appropriate name.
+        const customEvent = new CustomEvent("crimeChosen", {
+            detail: {
+                crimeThatWasChosen: event.target.value,
+
+            }
+        })
+
+        // Dispatch to event hub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+
+// export const ConvictionSelect = () => {
+//     // Get all convictions from application state
+//     getConvictions().then(() => {
+//         const convictions = useConvictions();
+//         render(convictions)
+//     })
+// }
+
+// const render = convictionsCollection => {
+//         /*
+//             Use interpolation here to invoke the map() method on
+//             the convictionsCollection to generate the option elements.
+//             Look back at the example provided above.
+//         */
+//         contentTarget.innerHTML = `
+//             <fieldset>
+//                 <legend>Conviction List </legend>
+//                     <select class="dropdown" id="crimeSelect">
+//                         <option value="0">Please select a crime...</option>
+//                 ${
+//                     convictionsCollection.map(convictionObject =>{
+//                         const conviction = convictionObject.name
+//                         return `<option>${conviction}</option>`
+//                     })
+//                 }
+//                         </select>
+//              </fieldset> 
+//         `
+// }
+
 export const ConvictionSelect = () => {
-    // Get all convictions from application state
-    getConvictions().then(() => {
-        const convictions = useConvictions();
-        render(convictions)
-    })
+    //     // Get all convictions from application state
+    getConvictions()
+        .then(() => {
+            const convictionsArray = useConvictions();
+            render(convictionsArray)
+        })
 }
 
-const render = convictionsCollection => {
-        /*
-            Use interpolation here to invoke the map() method on
-            the convictionsCollection to generate the option elements.
-            Look back at the example provided above.
-        */
+
+const render = (theConvictionsArray) => {
         contentTarget.innerHTML = `
-            <fieldset>
-                <legend>Conviction List </legend>
-                    <select class="dropdown" id="crimeSelect">
-                        <option value="0">Please select a crime...</option>
-                ${
-                    convictionsCollection.map(convictionObject =>{
-                        const conviction = convictionObject.name
-                        return `<option>${conviction}</option>`
-                    })
-                }
-                        </select>
-             </fieldset> 
-        `
+
+        <fieldset>
+             <legend> Conviction List </legend> 
+             <select class = "dropdown" id = "crimeSelect">
+                 <option value = "0"> Please select a crime... </option>
+                    ${
+                         theConvictionsArray.map(convictionObject => {
+                             return `<option value="${convictionObject.name}">${convictionObject.name}</option>`
+                           }).join("") 
+                    }
+            </select >
+
+        </fieldset>
+`
 }
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -6,6 +6,35 @@ import {
     CriminalHTML
 } from './CrimininalComponent.js'
 
+const eventHub = document.querySelector(".container")
+
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener("crimeChosen", event => {
+    // Use the property you added to the event detail.
+    if ("crimeId" in event.detail) {
+        console.log(event.detail.crimeThatWasChosen);
+
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+        const unfilteredCriminals = useCriminals();
+        const filteredCriminals = useCriminals().filter(criminal => {
+            return criminal.conviction === event.detail.crimeThatWasChosen
+        })
+
+        /*
+            Then invoke render() and pass the filtered collection as
+            an argument
+        */
+        addCriminalsToDom(filteredCriminals);
+
+    }
+})
+
+// const render = criminalCollection => {
+//     contentTarget.innerHTML =
+// }
+
 export const CriminalList = () => {
     getCriminals()
         .then(() => {
@@ -20,8 +49,8 @@ export const CriminalList = () => {
 const addCriminalsToDom = (theCriminalArray) => {
     const domCriminalElement = document.querySelector(".criminalsContainer");
     let CriminalHTMLArray = theCriminalArray.map(criminal => {
-        return CriminalHTML(criminal);
-    })
-    console.log("CriminalHTMLArray", CriminalHTMLArray);
-    domCriminalElement.innerHTML = CriminalHTMLArray.join("");
+            return CriminalHTML(criminal);
+        })
+        // console.log("CriminalHTMLArray", CriminalHTMLArray);
+    domCriminalElement.innerHTML = CriminalHTMLArray.join(" ");
 }
